416e3813cfadb0b351d67f518dc321c7b5ec1da0
Danny Chen <dannyisadog10@gmail.com>
2024-09-04 20:26:53 +0800

    feat(page): adjust forgot password form text to chinese

--- src/app/forgotPassword/page.tsx
+++ src/app/forgotPassword/page.tsx
@@ -4,7 +4,7 @@ import Title from "components/client/Title";
 export default async function Signin() {
   return (
     <>
-      <Title text="Forgot password" hasGoBack />
+      <Title text="忘記密碼" hasGoBack />
       <SendForgotPasswordLinkForm />
     </>
   );
