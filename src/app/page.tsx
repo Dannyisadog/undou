bd40f8391d60a552985a0146159aded0fd66f414
Danny Chen <dannyisadog10@gmail.com>
2024-09-04 20:25:55 +0800

    feat(page): adjust home page text to chinese

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import SignoutButton from "components/client/SignoutButton";
 import { auth, CustomSession } from "auth";
 import { redirect } from "next/navigation";
-import { Alert, AlertTitle, Stack, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import Title from "components/client/Title";
 import UserAvatar from "components/client/UserAvatar";
 import Provider from "providers/Provider";
@@ -17,20 +17,20 @@ export default async function Home() {
 
   return (
     <Provider session={session}>
-      <Title text="Basic Info" />
+      <Title text="基本資訊" />
       <UserAvatar />
       <Stack direction="row" spacing={2}>
-        <Typography textAlign="center">Account:</Typography>
+        <Typography textAlign="center">帳號:</Typography>
         <Typography textAlign="center">{session.authUser.email}</Typography>
       </Stack>
       <Stack direction="row" spacing={2}>
-        <Typography textAlign="center">Name:</Typography>
+        <Typography textAlign="center">使用者名稱:</Typography>
         <Typography textAlign="center">{session.authUser.name}</Typography>
       </Stack>
       <Stack direction="row" spacing={2}>
-        <Typography textAlign="center">Status:</Typography>
+        <Typography textAlign="center">狀態:</Typography>
         <Typography textAlign="center">
-          {emailVerified ? "Verified" : "Not Verified"}
+          {emailVerified ? "已驗證" : "尚未驗證"}
         </Typography>
       </Stack>
       <SignoutButton />
