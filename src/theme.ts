faff7c1d00bcd3ca4f61d353e4574e09ba83ad54
Danny Chen <dannyisadog10@gmail.com>
2024-09-04 20:03:50 +0800

    chore(theme): format file

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
 import { createTheme } from "@mui/material";
 import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  display: 'swap',
-  variable: '--font-poppins',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 const { fontFamily } = poppins.style;
@@ -26,7 +26,7 @@ export const theme = createTheme({
     },
     h5: {
       fontWeight: 600,
-    }
+    },
   },
   components: {
     MuiButton: {
