a46472358e2e24af13304c67ce0e23cac4444ceb
DannyChen <dannychen@kkcompany.com>
2024-09-05 12:59:48 +0800

    feat(page): restyle home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import SignoutButton from "components/client/SignoutButton";
 import { auth, CustomSession } from "auth";
 import { redirect } from "next/navigation";
-import { Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import Title from "components/client/Title";
 import UserAvatar from "components/client/UserAvatar";
 import Provider from "providers/Provider";
@@ -17,23 +17,27 @@ export default async function Home() {
 
   return (
     <Provider session={session}>
-      <Title text="基本資訊" />
-      <UserAvatar />
-      <Stack direction="row" spacing={2}>
-        <Typography textAlign="center">帳號:</Typography>
-        <Typography textAlign="center">{session.authUser.email}</Typography>
+      <Stack pt={4} width="100%" alignItems="center">
+        <Stack width={400} spacing={2} px={2}>
+          <Title text="基本資訊" hasGoBack={false} />
+          <UserAvatar />
+          <Stack direction="row" spacing={2}>
+            <Typography textAlign="center">帳號:</Typography>
+            <Typography textAlign="center">{session.authUser.email}</Typography>
+          </Stack>
+          <Stack direction="row" spacing={2}>
+            <Typography textAlign="center">使用者名稱:</Typography>
+            <Typography textAlign="center">{session.authUser.name}</Typography>
+          </Stack>
+          <Stack direction="row" spacing={2}>
+            <Typography textAlign="center">狀態:</Typography>
+            <Typography textAlign="center">
+              {emailVerified ? "已驗證" : "尚未驗證"}
+            </Typography>
+          </Stack>
+          <SignoutButton />
+        </Stack>
       </Stack>
-      <Stack direction="row" spacing={2}>
-        <Typography textAlign="center">使用者名稱:</Typography>
-        <Typography textAlign="center">{session.authUser.name}</Typography>
-      </Stack>
-      <Stack direction="row" spacing={2}>
-        <Typography textAlign="center">狀態:</Typography>
-        <Typography textAlign="center">
-          {emailVerified ? "已驗證" : "尚未驗證"}
-        </Typography>
-      </Stack>
-      <SignoutButton />
     </Provider>
   );
 }
