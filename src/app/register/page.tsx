1bba4efe98d33a7d6edf4e569708d4dd0a11ba99
DannyChen <dannychen@kkcompany.com>
2024-09-05 13:01:14 +0800

    feat(page): restyle register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,11 +1,22 @@
+import { Stack } from "@mui/material";
 import RegisterForm from "components/client/RegisterForm";
 import Title from "components/client/Title";
+import AuthLayout from "layout/AuthLayout";
 
-export default async function Signin() {
+export default async function RegisterPage() {
   return (
-    <>
-      <Title text="註冊" hasGoBack />
-      <RegisterForm />
-    </>
+    <AuthLayout>
+      <Stack
+        sx={{
+          alignItems: "center",
+        }}
+        pt={4}
+      >
+        <Stack width="100%" maxWidth={400} spacing={2} px={2}>
+          <Title text="註冊" hasGoBack />
+          <RegisterForm />
+        </Stack>
+      </Stack>
+    </AuthLayout>
   );
 }
