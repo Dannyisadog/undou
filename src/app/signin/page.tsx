89effe7d07d21e827aa3cd6b7377686360463319
DannyChen <dannychen@kkcompany.com>
2024-09-05 13:27:09 +0800

    feat(page): add signin page tab title

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -2,6 +2,11 @@ import { auth } from "auth";
 import { redirect } from "next/navigation";
 import AuthLayout from "layout/AuthLayout";
 import Signin from "components/client/signin";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "運動火腿 - 登入",
+};
 
 export default async function SigninPage() {
   const session = await auth();
