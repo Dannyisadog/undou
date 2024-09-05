7f666879da13cd482017a011b01f4c8deb0f09b7
Danny Chen <dannyisadog10@gmail.com>
2024-09-05 22:14:03 +0800

    feat(component): add sidebar item component

---
+++ src/components/client/common/SidebarItem.tsx
@@ -0,0 +1,50 @@
+import { ListItem, ListItemButton, Typography } from "@mui/material";
+import { PRIMARY } from "colors";
+import Link from "next/link";
+
+interface SidebarItemProps {
+  text: string;
+  active?: boolean;
+  href: string;
+  onClick?: () => void;
+  icon?: React.ReactNode;
+}
+
+export default function SidebarItem(props: SidebarItemProps) {
+  const { text, active, href, onClick, icon } = props;
+
+  const getHref = (type: string) => {
+    return type === "all" ? "/" : `/?type=${type}`;
+  };
+
+  return (
+    <Link href={href} onClick={onClick}>
+      <ListItem
+        sx={{
+          height: 48,
+        }}
+      >
+        <ListItemButton
+          sx={{
+            py: 0.5,
+            borderRadius: 5,
+            backgroundColor: active ? "white" : "transparent",
+            color: active ? PRIMARY.main : "white",
+            "&:hover": {
+              backgroundColor: active ? "white" : "rgba(255, 255, 255, 0.15)",
+            },
+          }}
+        >
+          {icon}
+          <Typography
+            fontWeight={active ? 600 : "normal"}
+            fontSize={active ? "1.2rem" : "1rem"}
+            ml={icon ? 1 : 0}
+          >
+            {text}
+          </Typography>
+        </ListItemButton>
+      </ListItem>
+    </Link>
+  );
+}
