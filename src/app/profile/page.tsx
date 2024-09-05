29ea96803c315cadf6337559661a6412a746ec72
Danny Chen <dannyisadog10@gmail.com>
2024-09-05 20:45:30 +0800

    feat(page): restyle profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -24,30 +24,24 @@ export default async function ProfilePage() {
   return (
     <Provider session={session}>
       <GeneralLayout>
-        <Stack pt={4} width="100%" alignItems="center">
-          <Stack width={400} spacing={2} px={4}>
-            <Title text="基本資訊" hasGoBack={false} />
-            <UserAvatar />
-            <Stack direction="row" spacing={2}>
-              <Typography textAlign="center">帳號:</Typography>
-              <Typography textAlign="center">
-                {session.authUser.email}
-              </Typography>
-            </Stack>
-            <Stack direction="row" spacing={2}>
-              <Typography textAlign="center">使用者名稱:</Typography>
-              <Typography textAlign="center">
-                {session.authUser.name}
-              </Typography>
-            </Stack>
-            <Stack direction="row" spacing={2}>
-              <Typography textAlign="center">狀態:</Typography>
-              <Typography textAlign="center">
-                {emailVerified ? "已驗證" : "尚未驗證"}
-              </Typography>
-            </Stack>
-            <SignoutButton />
+        <Stack width={400} spacing={2} px={4}>
+          <Title text="基本資訊" hasGoBack={false} />
+          <UserAvatar />
+          <Stack direction="row" spacing={2}>
+            <Typography textAlign="center">帳號:</Typography>
+            <Typography textAlign="center">{session.authUser.email}</Typography>
           </Stack>
+          <Stack direction="row" spacing={2}>
+            <Typography textAlign="center">使用者名稱:</Typography>
+            <Typography textAlign="center">{session.authUser.name}</Typography>
+          </Stack>
+          <Stack direction="row" spacing={2}>
+            <Typography textAlign="center">狀態:</Typography>
+            <Typography textAlign="center">
+              {emailVerified ? "已驗證" : "尚未驗證"}
+            </Typography>
+          </Stack>
+          <SignoutButton />
         </Stack>
       </GeneralLayout>
     </Provider>
