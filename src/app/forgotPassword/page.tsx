019102e29af002a62db1bb9a1903bfa8bc90fd1e
DannyChen <dannychen@kkcompany.com>
2024-09-05 13:27:53 +0800

    feat(page): add forgot password page tab title

--- src/app/forgotPassword/page.tsx
+++ src/app/forgotPassword/page.tsx
@@ -2,6 +2,11 @@ import { Box, Stack } from "@mui/material";
 import SendForgotPasswordLinkForm from "components/client/SendForgotPasswordLinkForm";
 import Title from "components/client/Title";
 import AuthLayout from "layout/AuthLayout";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "運動火腿 - 忘記密碼",
+};
 
 export default async function Signin() {
   return (
