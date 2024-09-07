273c360c68230cbc7df0305fcf591f74204856fd
DannyChen <dannychen@kkcompany.com>
2024-09-07 22:32:41 +0800

    feat(page): remove provider in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
-import { auth, CustomSession } from "auth";
 import ActivityList from "components/client/Activities/ActivityList";
-import Title from "components/client/Title";
 import GeneralLayout from "layout/GeneralLayout";
-import { redirect } from "next/navigation";
-import Provider from "providers/Provider";
 
 export default async function Home() {
-  const session = (await auth()) as CustomSession;
-
   return (
-    <Provider session={session}>
-      <GeneralLayout>
-        <ActivityList />
-      </GeneralLayout>
-    </Provider>
+    <GeneralLayout>
+      <ActivityList />
+    </GeneralLayout>
   );
 }
