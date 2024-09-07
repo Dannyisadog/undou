8f7980230d6cedc94f449830702a3ac74fa37063
DannyChen <dannychen@kkcompany.com>
2024-09-07 22:36:48 +0800

    chore(provider): delete provider

--- src/providers/constants.tsx
+++
@@ -1,9 +0,0 @@
-import { CustomSession } from "auth";
-import { User } from "next-auth";
-
-export interface ProviderContextType {
-  session?: CustomSession;
-  updateSession: () => void;
-  users: User[];
-  updateUsers: () => void;
-}
