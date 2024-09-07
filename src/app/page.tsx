31968f5db7e1f50c2b70bd9dccd5e7a4f21fd800
DannyChen <dannychen@kkcompany.com>
2024-09-07 22:33:28 +0800

    feat(page): add home page tab title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import ActivityList from "components/client/Activities/ActivityList";
 import GeneralLayout from "layout/GeneralLayout";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "運動火腿 - 活動列表",
+};
 
 export default async function Home() {
   return (
