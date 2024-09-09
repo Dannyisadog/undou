ddd228f8ef1bc7f757ffc2bcf9cdef8aaec35dc2
Danny Chen <dannyisadog10@gmail.com>
2024-09-09 23:47:28 +0800

    feat(page): add tab title in activity created page

--- src/app/activity/created/page.tsx
+++ src/app/activity/created/page.tsx
@@ -1,5 +1,11 @@
+import CreatedActivity from "components/client/Activities/CreatedActivity";
 import Title from "components/client/Title";
 import GeneralLayout from "layout/GeneralLayout";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "運動火腿 - 我發起的活動",
+};
 
 export default function CreatedActivityPage() {
   return (
