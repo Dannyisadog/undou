f9f22b7c10a543d971f51a4e52c30cd3dabc3e68
Danny Chen <dannyisadog10@gmail.com>
2024-09-09 23:47:43 +0800

    feat(page): add tab title in activity joined page

--- src/app/activity/joined/page.tsx
+++ src/app/activity/joined/page.tsx
@@ -1,5 +1,11 @@
+import JoinedActivity from "components/client/Activities/JoinedActivity";
 import Title from "components/client/Title";
 import GeneralLayout from "layout/GeneralLayout";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "運動火腿 - 已參加的活動",
+};
 
 export default function JoinedActivityPage() {
   return (
