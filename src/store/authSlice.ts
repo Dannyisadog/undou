d1ee542be7ce7d4650a6e4a2b17135303692bb9d
DannyChen <dannychen@kkcompany.com>
2024-09-09 14:07:07 +0800

    feat(store): add setState & default session into auth slice

--- src/store/authSlice.ts
+++ src/store/authSlice.ts
@@ -1,9 +1,12 @@
 import { CustomSession } from "auth";
+import { StateCreator } from "zustand";
 
 export interface AuthSlice {
   session?: CustomSession;
+  updateSession: (session: CustomSession) => void;
 }
 
-export const createAuthSlice = (session: CustomSession): AuthSlice => ({
+export const createAuthSlice = (set: (partial: AuthSlice | Partial<AuthSlice> | ((state: AuthSlice) => AuthSlice | Partial<AuthSlice>), replace?: boolean | undefined) => void, session: CustomSession): AuthSlice => ({
   session,
+  updateSession: (session: CustomSession) => set({ session }),
 });
