71556586f7fcac3d835a5b9b318ad9992e8c1121
Danny Chen <dannyisadog10@gmail.com>
2024-09-10 23:11:17 +0800

    feat(api): add disjoin activity api

---
+++ src/app/api/activities/disjoin/[id]/route.ts
@@ -0,0 +1,23 @@
+import { disjoin } from "app/repository/activity";
+import { auth } from "auth";
+import { NextResponse } from "next/server";
+
+export const POST = auth(async (req, { params }) => {
+  if (req.auth) {
+    try {
+      const activityId = parseInt(params?.id as string);
+      await disjoin(activityId);
+      return NextResponse.json(
+        { message: "Activity disjoined" },
+        { status: 201 }
+      );
+    } catch (error) {
+      return NextResponse.json(
+        { message: (error as Error).message },
+        { status: 400 }
+      );
+    }
+  }
+
+  return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
+});
