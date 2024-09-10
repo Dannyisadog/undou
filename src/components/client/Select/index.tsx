25c6a4b19a01fe74acd0546d9107cb1c8d6cb349
Danny Chen <dannyisadog10@gmail.com>
2024-09-10 12:43:43 +0800

    feat(component): restyle select component

--- src/components/client/Select/index.tsx
+++ src/components/client/Select/index.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { Select as MuiSelect, MenuItem as MuiMenuItem } from "@mui/material";
+import {
+  Select as MuiSelect,
+  MenuItem as MuiMenuItem,
+  Stack,
+  Typography,
+  FormHelperText,
+} from "@mui/material";
+import { DARK_BLUE } from "colors";
 
 interface SelectProps<T> {
   items: T[];
@@ -9,32 +16,58 @@ interface SelectProps<T> {
   renderLabel: (value: T) => string;
   fullWidth?: boolean;
   placeholder?: string;
+  error?: boolean;
+  helperText?: string;
 }
 
 type MenuItemValue<T> = T extends string | number ? T : never;
 
 export default function Select<T>(props: SelectProps<T>) {
-  const { items, onChange, label, renderLabel, placeholder, ...reset } = props;
+  const {
+    items,
+    onChange,
+    label,
+    renderLabel,
+    placeholder,
+    helperText,
+    ...reset
+  } = props;
   return (
-    <MuiSelect
-      {...reset}
-      displayEmpty
-      onChange={(e) => {
-        onChange(e.target.value as T);
-      }}
-      renderValue={(selected: T) => {
-        if (!selected) {
-          return placeholder;
-        }
+    <Stack width="100%" spacing={0.5}>
+      {label && (
+        <Typography fontWeight="bold" variant="body2" color={DARK_BLUE}>
+          {label}
+        </Typography>
+      )}
+      <MuiSelect
+        {...reset}
+        displayEmpty
+        onChange={(e) => {
+          onChange(e.target.value as T);
+        }}
+        renderValue={(selected: T) => {
+          if (!selected) {
+            return placeholder;
+          }
 
-        return renderLabel(selected);
-      }}
-    >
-      {items.map((item, index) => (
-        <MuiMenuItem key={index} value={item as MenuItemValue<T>}>
-          {renderLabel(item)}
-        </MuiMenuItem>
-      ))}
-    </MuiSelect>
+          return renderLabel(selected);
+        }}
+      >
+        {items.map((item, index) => (
+          <MuiMenuItem key={index} value={item as MenuItemValue<T>}>
+            {renderLabel(item)}
+          </MuiMenuItem>
+        ))}
+      </MuiSelect>
+      {helperText && (
+        <FormHelperText
+          sx={{
+            color: "#d32f2f",
+          }}
+        >
+          {helperText}
+        </FormHelperText>
+      )}
+    </Stack>
   );
 }
