436e7d95a309a1b444a764f34a454a735ed7b70c
Danny Chen <dannyisadog10@gmail.com>
2024-09-11 14:03:12 +0800

    feat(component): display no data animation in joined page

--- src/components/client/Activities/JoinedActivity.tsx
+++ src/components/client/Activities/JoinedActivity.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { Activity } from "@prisma/client";
 import { redirect } from "next/navigation";
 import { useGlobalStore } from "providers/StoreProvider";
-import ActivityListSkeleton from "./ActivityListSkeleton";
 import { Stack, Typography } from "@mui/material";
 import Link from "next/link";
 import ActivityCard from "./ActivityCard";
 import Title from "../Title";
+import { ActivityWithParticipants } from "./ActivityInfo";
+import NoData from "../common/NoData";
 
 interface JoinedActivityProps {
-  activities: Activity[];
+  activities: ActivityWithParticipants[];
 }
 
 export default function JoinedActivity(props: JoinedActivityProps) {
@@ -25,7 +25,7 @@ export default function JoinedActivity(props: JoinedActivityProps) {
   return (
     <Stack width="100%" spacing={4}>
       <Title text="已參加的活動" hasGoBack={false} />
-      {activities.length === 0 && <Typography>目前沒有任何活動</Typography>}
+      {activities.length === 0 && <NoData />}
       <Stack spacing={2}>
         {activities.map((activity) => (
           <Link href={`/activity/${activity.id}`} key={activity.id}>
