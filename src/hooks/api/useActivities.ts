39cb7a39b4fe45e6ce550799c0c27b16ad7bdc94
Danny Chen <dannyisadog10@gmail.com>
2024-09-12 00:53:38 +0800

    fix(hook): fix useActivities hook with type

--- src/hooks/api/useActivities.ts
+++ src/hooks/api/useActivities.ts
@@ -1,11 +1,11 @@
 import { useQuery } from "react-query";
 import { ActivityWithParticipants } from "components/client/Activities/ActivityInfo";
 
-export const useActivities = () => {
+export const useActivities = (type: string) => {
   return useQuery<ActivityWithParticipants[]>({
-    queryKey: ["activities"],
+    queryKey: ["activities", type],
     queryFn: async () => {
-      const response = await fetch(`/api/activities/`);
+      const response = await fetch(`/api/activities/?type=${type}`);
       return response.json();
     },
   });
