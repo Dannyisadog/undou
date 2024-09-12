c7cd17488f8209885704c73a3091339217709fdc
Danny Chen <dannyisadog10@gmail.com>
2024-09-12 22:28:07 +0800

    feat(component): display joined users in activity info page

--- src/components/client/Activities/ActivityInfo.tsx
+++ src/components/client/Activities/ActivityInfo.tsx
@@ -23,9 +23,14 @@ import CelebrationIcon from "@mui/icons-material/Celebration";
 import AssignmentIcon from "@mui/icons-material/Assignment";
 import dayjs from "dayjs";
 import { items } from "../common/Sidebar";
+import UserList from "./UserList";
+
+export type ParticipantWithUser = Participant & {
+  user: User;
+};
 
 export type ActivityWithParticipants = Activity & {
-  participants: Participant[];
+  participants: ParticipantWithUser[];
 };
 
 export default function ActivityInfo() {
@@ -57,9 +62,7 @@ export default function ActivityInfo() {
     (participant) => !participant.is_deleted && participant.userId === user?.id
   );
 
-  const { isMobileSize } = useScreenSize();
-
-  const isOwner = activity?.creatorId === user?.id;
+  const { isDesktopSize, isMobileSize } = useScreenSize();
 
   const handleJoin = async () => {
     await joinActivityMutation.mutateAsync(id as string);
@@ -77,40 +80,67 @@ export default function ActivityInfo() {
 
   const item = items.find((item) => item.type === activity?.type);
 
+  const joinedUser =
+    activity?.participants.map((participant) => {
+      return participant.user;
+    }) ?? [];
+
+  user && joinedUser.unshift(user);
+
   return activity ? (
-    <Stack pt={isMobileSize ? 0 : 4}>
+    <Stack pt={isMobileSize ? 0 : 4} width="100%">
       <Title text={activity.name} />
-      <Stack spacing={2} pt={2} width="100%" mt={2}>
-        <TextItem
-          label="活動類型"
-          text={item?.label ?? "未知"}
-          icon={<CelebrationIcon color="primary" />}
-        />
-        <TextItem
-          label="地點"
-          text={activity.location}
-          icon={<LocationOnIcon color="primary" />}
-        />
-        <TextItem
-          label="費用 (現場付款)"
-          text={activity.fee.toString()}
-          icon={<AttachMoneyIcon color="primary" />}
-        />
-        <TextItem
-          label="開始時間"
-          text={dayjs(activity.startDate).format("YYYY-MM-DD HH:mm")}
-          icon={<CalendarMonthIcon color="primary" />}
-        />
-        <TextItem
-          label="結束時間"
-          text={dayjs(activity.endDate).format("YYYY-MM-DD HH:mm")}
-          icon={<CalendarMonthIcon color="primary" />}
-        />
-        <TextItem
-          label="活動描述"
-          text={activity.description}
-          icon={<AssignmentIcon color="primary" />}
-        />
+      <Stack
+        mt={2}
+        direction={isMobileSize ? "column" : "row"}
+        spacing={isMobileSize ? 6 : 0}
+        width="100%"
+        maxWidth="1080px"
+        sx={{
+          p: 3,
+          borderRadius: 4,
+          backgroundColor: "white",
+        }}
+      >
+        <Stack spacing={2} width="100%">
+          <Typography variant="h5">基本資訊</Typography>
+          <TextItem
+            label="活動類型"
+            text={item?.label ?? "未知"}
+            icon={<CelebrationIcon color="primary" />}
+          />
+          <TextItem
+            label="地點"
+            text={activity.location}
+            icon={<LocationOnIcon color="primary" />}
+          />
+          <TextItem
+            label="費用 (現場付款)"
+            text={activity.fee.toString()}
+            icon={<AttachMoneyIcon color="primary" />}
+          />
+          <TextItem
+            label="開始時間"
+            text={dayjs(activity.startDate).format("YYYY-MM-DD HH:mm")}
+            icon={<CalendarMonthIcon color="primary" />}
+          />
+          <TextItem
+            label="結束時間"
+            text={dayjs(activity.endDate).format("YYYY-MM-DD HH:mm")}
+            icon={<CalendarMonthIcon color="primary" />}
+          />
+          <TextItem
+            label="活動描述"
+            text={activity.description}
+            icon={<AssignmentIcon color="primary" />}
+          />
+        </Stack>
+        {
+          <Stack spacing={2} width="100%">
+            <Typography variant="h5">參加名單</Typography>
+            <UserList users={joinedUser} />
+          </Stack>
+        }
       </Stack>
       <ActivityInfoFooter
         handleJoin={handleJoin}
