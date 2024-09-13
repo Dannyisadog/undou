94429459353eaf7283cc13ef19b358f7f8d29714
Danny Chen <dannyisadog10@gmail.com>
2024-09-13 11:10:03 +0800

    feat(component): add city select component

---
+++ src/components/client/CitySelect.tsx
@@ -0,0 +1,29 @@
+import { getCities } from "data/cities";
+import Select from "./Select";
+
+interface CitySelectProps {
+  error?: boolean;
+  helperText?: string;
+  onChange: (value: string) => void;
+}
+
+export default function CitySelect(props: CitySelectProps) {
+  const { error, helperText, onChange } = props;
+
+  const cities = getCities();
+
+  const cityList = cities.map((city) => city.CityName);
+
+  return (
+    <Select
+      fullWidth
+      placeholder="請選擇活動縣市"
+      label="縣市"
+      error={error}
+      helperText={helperText}
+      items={cityList}
+      renderLabel={(value) => value}
+      onChange={onChange}
+    />
+  );
+}
